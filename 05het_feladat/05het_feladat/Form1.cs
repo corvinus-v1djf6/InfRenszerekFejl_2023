0dd1716a607fddecf9263119cc88e1e0da08711b
Balázs Schmidt <balazs.schmidt2@stud.uni-corvinus.hu>
2023-03-28 09:38:02 +0200

    egesz05het

---
+++ 05het_feladat/05het_feladat/Form1.cs
@@ -0,0 +1,104 @@
+﻿using _05het_feladat.Entities;
+using _05het_feladat.MnbServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
+
+namespace _05het_feladat
+{
+    public partial class Form1 : Form
+    {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.DataSource = Rates;
+
+            var r = GetRates();
+            GetXmlData(r);        //GetXmlData(GetRates());
+
+            PrintChart();
+        }
+
+        private void PrintChart()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void GetXmlData(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                var date = item.GetAttribute("date");
+
+                var rate = (XmlElement)item.ChildNodes[0];
+                var currency = rate.GetAttribute("curr");
+                var unit = int.Parse(rate.GetAttribute("unit"));
+                var value = decimal.Parse(rate.InnerText);
+
+
+                Rates.Add(new RateData()
+                {
+                    Date = DateTime.Parse(date),
+                    Currency = currency,
+                    Value = unit !=0
+                            ? value / unit
+                            : 0
+
+                    //    if (unit != 0)
+                    //    rate.Value = value / unit;
+
+                });
+                
+
+            }
+        }
+
+        private string GetRates()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            return result;
+
+        }
+
+    }
+}

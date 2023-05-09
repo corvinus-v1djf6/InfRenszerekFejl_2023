41339883d9a87720ede33ee2180dddeb927acb2d
Balázs Schmidt <balazs.schmidt2@stud.uni-corvinus.hu>
2023-05-09 09:30:03 +0200

    gyak09

---
+++ gyakorlat_week09/gyakorlat_week09/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakorlat_week09.Entities
+{
+    public class DeathProbability
+    {
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public double HalalValoszinuseg { get; set; }
+    }
+}
---
+++ gyakorlat_week09/gyakorlat_week09/Form1.cs
@@ -0,0 +1,112 @@
+﻿using gyakorlat_week09.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyakorlat_week09
+{
+    public partial class Form1 : Form
+    {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
+        Random rng = new Random(1234);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Population = GetPopulation(@"C:\Temp\nép.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+        }
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+ 
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
+
+
+
+
+
+        // Végigmegyünk a vizsgált éveken
+        for (int year = 2005; year <= 2024; year++)
+        {
+           
+            // Végigmegyünk az összes személyen
+            for (int i = 0; i<Population.Count; i++)
+            {
+            // Ide jön a szimulációs lépés
+            //SimStep(year, Person);
+            }
+
+            int nbrOfMales = (from x in Population
+                              where x.Gender == Gender.Male && x.IsAlive
+                              select x).Count();
+            int nbrOfFemales = (from x in Population
+                            where x.Gender == Gender.Female && x.IsAlive
+                            select x).Count();
+        Console.WriteLine(
+            string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+        }
+    }
+ 
+}
+
